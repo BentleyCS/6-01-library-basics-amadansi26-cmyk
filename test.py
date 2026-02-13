62ac71542e7b85068edca7ffb4f049ba2661f327
amadansi26-cmyk <amadansi26@bentleyschool.org>
2026-02-13 11:26:37 -0800

    Update test.py

--- test.py
+++ test.py
@@ -1,24 +1,21 @@
-import unittest
-import main
+import CSP_6_01_Library_Basics as HW
+
+def test_process_expenses():
+    assert HW.process_expenses([100,20])==[115.0, 23.0]
 
 
-class TestMainFunctions(unittest.TestCase):
-
-    def test_process_expenses(self):
-        prices = [47.50, 189.99, 320.40]
-        result = main.process_expenses(prices)
-        self.assertEqual(result, [54.625, 218.4885, 368.46])
-
-    def test_sanitize_usernames(self):
-        usernames = ["  DeltaForce  ", " Echo_Leader ", "  FoXTRot99"]
-        result = main.sanitize_usernames(usernames)
-        self.assertEqual(result, ["deltaforce", "echo_leader", "foxtrot99"])
-
-    def test_identify_outliers(self):
-        numbers = [12, 455, 87, 1024, 76, 305]
-        result = main.identify_outliers(numbers)
-        self.assertEqual(result, [455, 1024, 305])
+def test_analyze_scores():
+    assert HW.analyze_scores([100,50,75]) ==[100, 75.0]
 
 
-if __name__ == "__main__":
-    unittest.main()
+def test_sanitize_usernames():
+    assert HW.sanitize_usernames("John Mack" "Albert K")
+
+
+def test_identify_outliers():
+    assert HW.identify_outliers([101, 90, 200]) == [100, 200]
+
+
+def test_search_and_report():
+    assert HW.search_and_report(["  Apple", "Banana ", "  CHERRY  "], "banana") == 1
+    assert HW.search_and_report(["  Apple", "Banana ", "  CHERRY  "], "pineapple") == -1
